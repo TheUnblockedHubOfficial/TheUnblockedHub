9bc8ae249f6fa26b3402a5d27dfb4c9750b5a77a
Bradyn DiBiase <mail@theunblockedhub.com>
2024-01-29 20:00:21 -0500

    fix

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 import express from 'express'
 import basicAuth from 'express-basic-auth'
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from '@vercel/speed-insights/next'
+import { Analytics } from '@vercel/analytics/react'
 import http from 'node:http'
 import { createBareServer } from '@tomphttp/bare-server-node'
 import path from 'node:path'
 import cors from 'cors'
 import config from './config.js'
-import { Analytics } from '@vercel/analytics/react';
 const __dirname = process.cwd()
 const server = http.createServer()
 const app = express(server)
